5a6e0f34983f0c84d4a4a15aea97ee4499283891
codegeekery <85375414+codegeekery@users.noreply.github.com>
2025-10-31 11:31:02 +0000

    fix serving static files

--- app/index.ts
+++ app/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
-import { join } from 'path';
-import router from './routes/routes.ts';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import router from './routes/routes.js';
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,24 +14,22 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// 🔧 Usa la raíz del proyecto para static y views
-app.use(express.static(join(process.cwd(), 'app', 'public')));
+// Archivos estáticos (usa __dirname)
+app.use(express.static(join(__dirname, 'public')));
 
 // EJS
 app.set('view engine', 'ejs');
-app.set('views', join(process.cwd(), 'app', 'views')); // <-- clave
+app.set('views', join(__dirname, 'views'));
 
 // Rutas
 app.use('/', router);
 
 // 404 (después de TODAS las rutas)
 app.use((req, res) => {
-  res.status(404).render('404'); // renderiza app/views/404.ejs
+  res.status(404).render('404');
 });
-
-
-
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
