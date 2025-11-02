e474386abce11d6dabdc7eb6febcd03a3770b9da
codegeekery <85375414+codegeekery@users.noreply.github.com>
2025-11-02 00:18:18 +0000

    adding new conector endpoint db

--- app/config/config.ts
+++ app/config/config.ts
@@ -1,11 +1,15 @@
 import { Client } from 'pg'
 
 const database = new Client({
-    connectionString: process.env.DATABASE_URL!,
+  connectionString: process.env.DATABASE_URL!,
+  ssl: {
+    rejectUnauthorized: false, // SSL sin verificación estricta del certificado
+  },
 })
 
 database.connect().catch((err) => {
-    console.error('Error connecting to the database', err.stack)
+  console.error('Error connecting to the database:', err.stack)
 })
 
 export const sql = database
+
